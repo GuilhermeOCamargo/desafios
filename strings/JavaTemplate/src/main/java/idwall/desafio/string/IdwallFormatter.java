fd6751f1fecdf58b9d85e884153208b2c12f1937
GuilhermeOCamargo <guilherme.ocamargo@gmail.com>
2020-02-08 00:16:35 -0300

    Criando formatacao simples

--- strings/JavaTemplate/src/main/java/idwall/desafio/string/IdwallFormatter.java
+++ strings/JavaTemplate/src/main/java/idwall/desafio/string/IdwallFormatter.java
@@ -1,18 +1,45 @@
 package idwall.desafio.string;
-
 /**
  * Created by Rodrigo Catão Araujo on 06/02/2018.
  */
 public class IdwallFormatter extends StringFormatter {
 
-    /**
-     * Should format as described in the challenge
-     *
-     * @param text
-     * @return
-     */
     @Override
     public String format(String text) {
-        throw new UnsupportedOperationException();
+        return handleFormat(text, getDefaultLimit());
+    }
+
+    @Override
+    public String format(String text, Integer limit){
+        return handleFormat(text, getApplicableLimit(limit));
+    }
+
+    private String handleFormat(String text, Integer limit){
+        validateText(text);
+        boolean hasNext = true;
+        String textSubstring;
+        while (hasNext){
+            if(text.length() >= limit){
+                textSubstring = text.substring(0, limit);
+                if(text.substring(0, limit+1).endsWith(" ") || text.substring(0, limit+1).endsWith("\n")){
+                    textSubstring = textSubstring.trim();
+                    text = text.substring(limit, text.length()).trim();
+                } else{
+                    if(textSubstring.lastIndexOf(" ") > textSubstring.lastIndexOf("\n"))
+                        textSubstring = textSubstring.substring(0, textSubstring.lastIndexOf(" "));
+                    else
+                        textSubstring = textSubstring.substring(0, textSubstring.lastIndexOf("\n"));
+
+                    text = text.substring(textSubstring.length(), text.length()).trim();
+                }
+                getBuilder().append(textSubstring)
+                        .append("\n");
+                hasNext = text.length() > 0;
+            }else{
+                getBuilder().append(text);
+                hasNext = false;
+            }
+        }
+        return getFormatedText();
     }
 }
--- strings/JavaTemplate/src/main/java/idwall/desafio/string/StringFormatter.java
+++ strings/JavaTemplate/src/main/java/idwall/desafio/string/StringFormatter.java
@@ -1,14 +1,21 @@
 package idwall.desafio.string;
 
+import com.google.common.base.Strings;
+
+import java.util.Objects;
+
 /**
  * Created by Rodrigo Catão Araujo on 06/02/2018.
  */
 public abstract class StringFormatter {
 
-    private Integer limit;
+    private int defaultLimit;
+    private StringBuilder builder;
+    private String formatedText;
 
     public StringFormatter() {
-        this.limit = 40;
+        this.defaultLimit = 40;
+        builder = new StringBuilder();
     }
 
     /**
@@ -18,4 +25,37 @@ public abstract class StringFormatter {
      * @return
      */
     public abstract String format(String text);
+    /**
+     * It receives a text and should return it formatted
+     *
+     * @param text
+     * @param limit
+     * @return
+     */
+    public abstract String format(String text, Integer limit);
+
+    protected Integer getDefaultLimit() {
+        return defaultLimit;
+    }
+
+    protected StringBuilder getBuilder() {
+        return builder;
+    }
+
+    protected Integer getApplicableLimit(Integer limit){
+        if(Objects.isNull(limit) || limit <= 0)
+            return defaultLimit;
+        return limit;
+    }
+
+    protected String getFormatedText() {
+        formatedText = builder.toString();
+        builder = new StringBuilder();
+        return formatedText;
+    }
+    protected void validateText(String text){
+        if(Strings.isNullOrEmpty(text))
+            throw new IllegalArgumentException("O texto nao pode ser vazio ou nulo");
+    }
+
 }
